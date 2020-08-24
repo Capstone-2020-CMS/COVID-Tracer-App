86c807c8e4e8ce8d4825b21254366ed2495ca462
RothvMartin <62974036+RothvMartin@users.noreply.github.com>
2020-08-24 16:31:52 +1200

    Still testing testing

--- Application/app/src/main/java/com/covid/utils/CodeManager.java
+++ Application/app/src/main/java/com/covid/utils/CodeManager.java
@@ -1,8 +1,20 @@
 package com.covid.utils;
 
+import java.security.MessageDigest;
+
 public class CodeManager {
 
     public static String generateCode(){
+
+
+
+        //Create a salt
+        //Generate a base code
+        //Salt the base code to create the (hopefully) unique ID
+        //Return the ID
+
+
+
         return "";
     }
 }
--- Application/app/src/test/java/com/covid/ExampleUnitTest.java
+++ Application/app/src/test/java/com/covid/ExampleUnitTest.java
@@ -4,6 +4,10 @@ import com.covid.utils.CodeManager;
 
 import org.junit.Test;
 
+import java.security.MessageDigest;
+import java.security.MessageDigestSpi;
+import java.util.Random;
+
 import static org.junit.Assert.*;
 
 /**
@@ -24,6 +28,36 @@ public class ExampleUnitTest {
         input = CodeManager.generateCode();
 
         assertEquals(16, input.length());
+    }
+
+
+    @Test
+    public void code_isGenerated(){
+
+
+        String salt = "SALT";
+        String bubbleID = "";
+        boolean hasRun = false;
+
+        int min = 65;
+        int max = 90;
+
+        for (int i = 0; i < 16; i++){
+            Random r = new Random();
+            int rNum = r.nextInt(max - min + 1) + min;
+
+            char newChar = (char) rNum;
+            bubbleID += newChar;
+        }
+
+        bubbleID += salt;
+
+        if(bubbleID.length() == 20){
+            hasRun = true;
+        }
+
+        assertEquals(true, hasRun);
+
     }
 
 }
