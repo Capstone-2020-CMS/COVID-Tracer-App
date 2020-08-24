33ed311f343a0d89358d6a2de2d301836431e4f2
RothvMartin <62974036+RothvMartin@users.noreply.github.com>
2020-08-24 13:54:32 +1200

    Add 2 tests

--- Application/app/src/test/java/com/covid/IDCodeTest.java
+++ Application/app/src/test/java/com/covid/IDCodeTest.java
@@ -1,4 +1,20 @@
 package com.covid;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class IDCodeTest {
+    @Test
+    public void code_isSize(){
+        //get a code of the correct size
+        assertEquals(16, appCode.getLength());
+    }
+
+    @Test
+    public void code_isSalted(){
+        //get code
+        //get salt
+        //de-hash code using salt
+    }
 }
