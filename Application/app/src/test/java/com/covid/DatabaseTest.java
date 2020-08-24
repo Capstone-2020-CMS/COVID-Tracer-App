04ef36b89c186e3321d2f2ad73e7c79beb181b2d
haywoc01 <haywoc01@myunitec.ac.nz>
2020-08-24 16:32:50 +1200

    Setup Robolectric
    Added roboletric to gradle.build files

---
+++ Application/app/src/test/java/com/covid/DatabaseTest.java
@@ -0,0 +1,59 @@
+package com.covid;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import java.io.File;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+
+@RunWith(RobolectricTestRunner.class)
+
+
+public class DatabaseTest {
+
+    //private EncountersDatabase database;
+
+
+
+    @Test
+    public void database_isCreated() throws Exception{
+
+    }
+
+    @Test
+    public void UniqueID_isAdded() throws Exception{
+
+    }
+
+    @Test
+    public void encounterDate_isAdded() throws Exception{
+
+    }
+
+    @Test
+    public void UniqueID_isRetrieved() throws Exception{
+
+    }
+
+    @Test
+    public void encounterDate_isRetrieved() throws Exception{
+
+    }
+
+    @After
+    public void tearDown() {
+        //dbHelper.clearDb();
+    }*/
+
+}
