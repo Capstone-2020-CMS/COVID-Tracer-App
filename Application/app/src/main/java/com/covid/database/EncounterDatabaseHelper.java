c3d26ed00221d2eeb5f12f5a2edf3f5a7d579bf3
haywoc01 <haywoc01@myunitec.ac.nz>
2020-08-26 10:54:22 +1200

    Insert Data method added
    Added insertData method, now testing

--- Application/app/src/main/java/com/covid/database/EncounterDatabaseHelper.java
+++ Application/app/src/main/java/com/covid/database/EncounterDatabaseHelper.java
@@ -1,5 +1,6 @@
 package com.covid.database;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
@@ -12,11 +13,12 @@ public class EncounterDatabaseHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "MyBubble.db";
     public static final String TABLE_NAME = "Encounters_Table";
     public static final String COL1 = "ID";
-    public static final String COL2 = "ENCOUNTERDATE";
+    public static final String COL2 = "ENCOUNTER_DATE";
+
+
 
     public EncounterDatabaseHelper(@Nullable Context context) {
         super(context, DATABASE_NAME, null, 1);
-        SQLiteDatabase db = getWritableDatabase();
     }
 
     public static boolean checkDataBaseExists(String DB_FULL_PATH) {
@@ -36,7 +38,7 @@ public class EncounterDatabaseHelper extends SQLiteOpenHelper {
         db.execSQL(" create table " + TABLE_NAME + "(ID TEXT PRIMARY KEY CHECK(\n" +
                 "        typeof(\"ID\") = \"text\" AND\n" +
                 "        length(\"ID\") <= 20\n" +
-                "    ), ENCOUNTERDATE DATE)");
+                "    ), ENCOUNTER_DATE TEXT)");
 
     }
 
@@ -44,5 +46,19 @@ public class EncounterDatabaseHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
+    }
+
+    public boolean insertData(String ID, String EncounterDate) {
+            SQLiteDatabase db = getWritableDatabase();
+            ContentValues newValues = new ContentValues();
+            newValues.put("ID", ID);
+            newValues.put("EncounterDate", EncounterDate);
+            long result = db.insert(TABLE_NAME, null, newValues);
+            if (result == -1) {
+                return false;
+            }
+            else {
+                return true;
+            }
     }
 }
--- Application/app/src/test/java/com/covid/DatabaseTest.java
+++ Application/app/src/test/java/com/covid/DatabaseTest.java
@@ -1,6 +1,8 @@
 package com.covid;
 import com.covid.database.EncounterDatabaseHelper;
 import android.content.Context;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Build;
 
 import org.junit.After;
@@ -27,39 +29,74 @@ import static org.junit.Assert.*;
 public class DatabaseTest {
 
     @Test
-    public void databaseExists_returnsFalse() throws Exception{
+    //Checking existence of database: Return False when database does not exist
+    public void databaseNotExists_returnsFalse() throws Exception{
+        //Check that passes when database does not exist
         boolean databaseExists = checkDataBaseExists("NonexistentDatabase.db");
         assertFalse(databaseExists != true);
     }
 
     @Test
+    //Checking existence of database: Return true when database does exist
     public void databaseExists_returnsTrue() throws Exception{
+        //Check that test passes when database exists
         boolean databaseExists = checkDataBaseExists("/data/data/com.covid/databases/MyBubble.db");
         assertTrue(databaseExists = true);
     }
 
 
   @Test
-    public void UniqueID_isAdded() throws Exception{
+  //Checking valid ID can be added to database: Return true when ID is added successfully
+    public void IDisAdded_returnsTrue() throws Exception{
+        //Check that test passes when an ID that is text CAN be added to the database
+        boolean IDisAdded = EncounterDatabaseHelper.insertData("ID567891235673dd2aQt", "");
+        assertTrue(IDisAdded = true);
+    }
+
+    @Test
+    //Checking invalid ID cannot be added to database: Return false when ID is not added
+    public void IDisAdded_returnsFalse() throws Exception{
+        //Check that test passes when an ID that is not text CANNOT be added to the database
+    }
+
+    @Test
+    //Checking valid ID can be added to database: Return true when ID is added successfully
+    public void IDisAddedLess20Char_returnsTrue() throws Exception{
+        //Check that test passes when an ID that is text AND less than 20 characters CAN be added to the database
 
     }
 
     @Test
-    public void encounterDate_isAdded() throws Exception{
+    //Checking valid ID can be added to database: Return true when ID is added successfully
+    public void IDisAddedEqual20Char_returnsTrue() throws Exception{
+        //Check that test passes when an ID that is text AND equal to 20 characters CAN be added to the database
 
     }
-
 
     @Test
-    public void UniqueID_isRetrieved() throws Exception{
+    //Checking invalid ID cannot be added to database: Return false when ID is not added
+    public void IDisAddedMore20Char_returnsFalse() throws Exception{
+        //Check that test passes when an ID that is text AND more than 20 characters CANNOT be added to the database
 
     }
 
 
-    @Test
-    public void encounterDate_isRetrieved() throws Exception{
-
-    }
+//    @Test
+//    public void encounterDate_isAdded() throws Exception{
+//
+//    }
+//
+//
+//    @Test
+//    public void UniqueID_isRetrieved() throws Exception{
+//
+//    }
+//
+//
+//    @Test
+//    public void encounterDate_isRetrieved() throws Exception{
+//
+//    }
 
 
 }
