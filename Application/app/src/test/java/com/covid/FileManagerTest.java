8105834834111caaa31e9f5c9f2af9a58dfa0179
RothvMartin <62974036+RothvMartin@users.noreply.github.com>
2020-08-26 15:55:06 +1200

    26/08/2020 Testing file manager write and read OK

--- Application/app/src/test/java/com/covid/FileManagerTest.java
+++ Application/app/src/test/java/com/covid/FileManagerTest.java
@@ -2,10 +2,13 @@ package com.covid;
 
 import org.junit.Test;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -35,17 +38,28 @@ public class FileManagerTest{
 
         String fileName = "testFile.txt";
 
+        List<String>stringList = new ArrayList<String>();
+
         try{
 
             FileReader fileReader = new FileReader(fileName);
 
-            FileWriter fileWriter = new FileWriter(testFile);
-            fileWriter.write("TEST");
-            fileWriter.close();
+            BufferedReader br = new BufferedReader(fileReader);
+
+            String line;
+
+            while ((line = br.readLine()) != null){
+                stringList.add(line);
+            }
+
+            br.close();
+            fileReader.close();
 
         }catch (Exception e){
             e.printStackTrace();
         }
+
+        assertEquals("TEST", stringList.get(0));
 
 
     }
@@ -55,78 +69,5 @@ public class FileManagerTest{
 }
 
 
-
-
-
-
-
-        /*    SimpleDateFormat dateformat = new SimpleDateFormat("yyyy-MM-dd'T'HH-mm-ss'Z'");
-
-    Date date = Calendar.getInstance().getTime();
-    String fileName = dateformat.format(date) + ".txt";
-
-    File test = new File(logPath, fileName);
-
-        try {
-        FileWriter fileWriter = new FileWriter(test);
-        fileWriter.write(input);
-        fileWriter.close();
-    } catch (java.io.IOException ex) {
-        ex.printStackTrace();
-    }*/
-
-
-/*        EditText textmsg;
-        static final int READ_BLOCK_SIZE = 100;
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-
-            textmsg=(EditText)findViewById(R.id.editText1);
-        }
-
-        // write text to file
-        public void WriteBtn(View v) {
-            // add-write text into file
-            try {
-                FileOutputStream fileout=openFileOutput("mytextfile.txt", MODE_PRIVATE);
-                OutputStreamWriter outputWriter=new OutputStreamWriter(fileout);
-                outputWriter.write(textmsg.getText().toString());
-                outputWriter.close();
-
-                //display file saved message
-                Toast.makeText(getBaseContext(), "File saved successfully!",
-                        Toast.LENGTH_SHORT).show();
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-        // Read text from file
-        public void ReadBtn(View v) {
-            //reading text from file
-            try {
-                FileInputStream fileIn=openFileInput("mytextfile.txt");
-                InputStreamReader InputRead= new InputStreamReader(fileIn);
-
-                char[] inputBuffer= new char[READ_BLOCK_SIZE];
-                String s="";
-                int charRead;
-
-                while ((charRead=InputRead.read(inputBuffer))>0) {
-                    // char to string conversion
-                    String readstring=String.copyValueOf(inputBuffer,0,charRead);
-                    s +=readstring;
-                }
-                InputRead.close();
-                textmsg.setText(s);
-
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }*/
 
 
