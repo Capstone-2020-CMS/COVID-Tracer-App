521bcdbfcab46b3c26ef3f0f2c96141fb859990a
haywoc01 <haywoc01@myunitec.ac.nz>
2020-08-27 12:33:16 +1200

    Added recordEncounters method
    record Encounters class contains method for recording encounters. This method is called in the Main method in onCreate

--- Application/app/src/main/java/com/covid/database/EncountersData.java
+++ Application/app/src/main/java/com/covid/database/EncountersData.java
@@ -8,7 +8,12 @@ public class EncountersData {
     public static void recordEncountersData() {
 
         //Placeholder insertEncounterData method
-        String btEncounter =  "test ID: 567891235673dd2aQt";
+        //String btEncounter =  "test ID: 567891235673dd2aQt";
+        //String date = "test Date: 27-08-2020 12:10:18pm";
+        //MainActivity.encounterDB.insertEncounterData(btEncounter, date);
+
+        //Recording encounters
+        String btEncounter =  "567891235673dd2aQt";
         String date = "test Date: 27-08-2020 12:10:18pm";
         MainActivity.encounterDB.insertEncounterData(btEncounter, date);
     }
