b9103a8d650a75bb7646fd0471f5a8cdec1431fc
Samuel Shingler <samuel.s.shingler@gmail.com>
2020-08-31 16:31:40 +1200

    Bluetooth foreground notification

--- Application/app/src/main/java/com/covid/bluetooth/BLEService.java
+++ Application/app/src/main/java/com/covid/bluetooth/BLEService.java
@@ -1,23 +1,27 @@
 package com.covid.bluetooth;
 
+import android.app.Notification;
 import android.app.Service;
 import android.bluetooth.le.ScanCallback;
 import android.bluetooth.le.ScanFilter;
 import android.bluetooth.le.ScanResult;
+import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
 
+import com.covid.R;
 import com.covid.utils.txtFile;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
-import java.util.ListIterator;
 
+import static com.covid.MainActivity.NOTIFICATION_CHANNEL;
 import static com.covid.MainActivity.bleScanner;
 import static com.covid.MainActivity.scanSettings;
 
@@ -36,13 +40,36 @@ public class BLEService extends Service {
     public int onStartCommand(Intent intent, int flags, int startId) {
         //bleScanner.startScan(scanFilters,scanSettings,callback);
         bleScanner.startScan(null,scanSettings,callback);
-        return super.onStartCommand(intent, flags, Service.START_STICKY);
+        startForeground(1, createForegroundNotification());
+        return Service.START_STICKY;
     }
 
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    private Notification createForegroundNotification() {
+        // Build a notification using bytesRead and contentLength
+
+        Context context = getApplicationContext();
+
+        SimpleDateFormat dateformat = new SimpleDateFormat("yyyy-MM-dd'T'HH-mm-ss'Z'");
+
+        Date date = Calendar.getInstance().getTime();
+
+        String title = dateformat.format(date);
+
+        Notification notification = new NotificationCompat.Builder(context, NOTIFICATION_CHANNEL)
+                .setContentTitle(title)
+                .setContentText("BLE scanning is active")
+                .setOngoing(true)
+                .setPriority(Notification.PRIORITY_DEFAULT)
+                .setSmallIcon(R.drawable.ic_notifications_black_24dp)
+                .build();
+
+        return notification;
     }
 
     private void createCallback() {
