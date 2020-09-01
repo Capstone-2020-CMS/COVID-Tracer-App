0377e8fff2b70e9000dbb9f29bfdd92cba1aef49
haywoc01 <haywoc01@myunitec.ac.nz>
2020-09-01 14:10:55 +1200

    Revert "Revert "Tidied up Encounters Data""
    This reverts commit c4a3e6a169b3a1e17d46568711b8ad399a8e47f8.

--- Application/app/src/main/java/com/covid/database/EncountersData.java
+++ Application/app/src/main/java/com/covid/database/EncountersData.java
@@ -13,22 +13,11 @@ public class EncountersData {
         //MainActivity.encounterDB.insertEncounterData(btEncounter, date);
 
         //Recording encounters (keep only 4 lines below when bluetooth is working, changing only the Strings content)
-        String btEncounterID =  "567891235673dd2aQt";
-        String btEncounterDate = "2020-02-15";
-        String btEncounterTime = "10:00";
+        String btEncounterID =  "567891235673dd2aQt"; //placeholder ID
+        String btEncounterDate = "2020-02-15"; // placeholder Date
+        String btEncounterTime = "10:00"; //placeholder Time
         MainActivity.encounterDB.insertEncounterData(btEncounterID, btEncounterDate, btEncounterTime);
-        String btEncounterID2 =  "4567891235673dd2aQt";
-        String btEncounterDate2 = "2020-09-01";
-        String btEncounterTime2 = "12:00";
-        MainActivity.encounterDB.insertEncounterData(btEncounterID2, btEncounterDate2, btEncounterTime2);
-        String btEncounterID3 =  "UpdatedID";
-        String btEncounterDate3 = "2020-08-30";
-        String btEncounterTime3 = "13:00";
-        MainActivity.encounterDB.insertEncounterData(btEncounterID3, btEncounterDate3, btEncounterTime3);
-        String btEncounterID4 =  "UpdatedID";
-        String btEncounterDate4 = "2020-09-01";
-        String btEncounterTime4 = "14:00";
-        MainActivity.encounterDB.insertEncounterData(btEncounterID4, btEncounterDate4, btEncounterTime4);
+
 
         //Deleting aged encounter data
         MainActivity.encounterDB.deletedAgedEncounterData();
