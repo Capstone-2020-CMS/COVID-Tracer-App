efdb8dc6db4e30bf385bb0afd62c6695a99c387c
haywoc01 <haywoc01@myunitec.ac.nz>
2020-09-01 14:02:10 +1200

    Update working on database
    Update working on database. If ID already exists, data is updated. The replace() method replaces the original data with the new data

--- Application/app/src/main/java/com/covid/database/EncountersData.java
+++ Application/app/src/main/java/com/covid/database/EncountersData.java
@@ -12,7 +12,7 @@ public class EncountersData {
         //String date = "test Date: 27-08-2020 12:10:18pm";
         //MainActivity.encounterDB.insertEncounterData(btEncounter, date);
 
-        //Recording encounters
+        //Recording encounters (keep only 4 lines below when bluetooth is working, changing only the Strings content)
         String btEncounterID =  "567891235673dd2aQt";
         String btEncounterDate = "2020-02-15";
         String btEncounterTime = "10:00";
@@ -21,6 +21,14 @@ public class EncountersData {
         String btEncounterDate2 = "2020-09-01";
         String btEncounterTime2 = "12:00";
         MainActivity.encounterDB.insertEncounterData(btEncounterID2, btEncounterDate2, btEncounterTime2);
+        String btEncounterID3 =  "UpdatedID";
+        String btEncounterDate3 = "2020-08-30";
+        String btEncounterTime3 = "13:00";
+        MainActivity.encounterDB.insertEncounterData(btEncounterID3, btEncounterDate3, btEncounterTime3);
+        String btEncounterID4 =  "UpdatedID";
+        String btEncounterDate4 = "2020-09-01";
+        String btEncounterTime4 = "14:00";
+        MainActivity.encounterDB.insertEncounterData(btEncounterID4, btEncounterDate4, btEncounterTime4);
 
         //Deleting aged encounter data
         MainActivity.encounterDB.deletedAgedEncounterData();
