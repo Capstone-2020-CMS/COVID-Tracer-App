1c268418b64503423072691c8c6b722aea5b51aa
haywoc01 <haywoc01@myunitec.ac.nz>
2020-09-14 17:45:24 +1200

    Added template for ResponseCloudData
    Added template for ResponseCloudData

--- Application/app/src/main/java/com/covid/database/ResponseCloudData.java
+++ Application/app/src/main/java/com/covid/database/ResponseCloudData.java
@@ -1,5 +1,20 @@
 package com.covid.database;
 
 public class ResponseCloudData {
+    String infectedUserEncounters;
 
+    public String getinfectedUserEncounters() {
+        return infectedUserEncounters;
+    }
+
+    public void setInfectedUsers(String greetings) {
+        this.infectedUserEncounters = infectedUserEncounters;
+    }
+
+    public ResponseCloudData(String greetings) {
+        this.infectedUserEncounters = greetings;
+    }
+
+    public ResponseCloudData() {
+    }
 }
