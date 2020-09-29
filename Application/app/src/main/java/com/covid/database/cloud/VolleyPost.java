8da110c27a72ad8953ea0655a345e52eef5fb869
haywoc01 <haywoc01@myunitec.ac.nz>
2020-09-29 17:48:10 +1300

    Martin's button "Confirm exposure" sends ID to cloud DB
    Connected the volleypost method to the "confirm exposure" button so that the personal ID is sent to the InfectedUsers database when the "confirm exposure" button is clicked.

--- Application/app/src/main/java/com/covid/database/cloud/VolleyPost.java
+++ Application/app/src/main/java/com/covid/database/cloud/VolleyPost.java
@@ -1,11 +1,12 @@
-package com.covid.database;
+package com.covid.database.cloud;
 
+import android.app.Service;
 import android.content.Context;
-import android.os.Bundle;
+import android.content.Intent;
+import android.os.IBinder;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+
+import androidx.annotation.Nullable;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.NetworkResponse;
@@ -15,82 +16,23 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.HttpHeaderParser;
-import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.covid.MainActivity;
 
-
-
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.UnsupportedEncodingException;
 
-
-public class CloudInfectedUsers extends MainActivity {
-    private Context context;
+public class VolleyPost{
 
 
-
-    //Default constructor
-    public CloudInfectedUsers(){
-        this.context = context;
-
-    }
-
-    //////////////////////////////////////////////////////////////////////////////////////////////////////////
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    /////////////////////////////////////////////////////////////////////////////////////////////////////*/
-
-//    public void sendIdToDB(View addUserToDB) {
-//        // Do something in response to button click
-//        Log.v("btnMsg", "IT WORKS!");
-//    }
-//
-//    RequestQueue requestQueue = Volley.newRequestQueue(context);
-//
-//
-//
-//
-//
-//    public void getInfectedUsers() {
-//
-//        String url = "https://s6bimnllqb.execute-api.ap-southeast-2.amazonaws.com/prod/data";
-//
-//
-//        JsonArrayRequest request;
-//
-//        {
-//            Response.Listener<JSONArray> responseListener = new Response.Listener<JSONArray>() {
-//                @Override
-//                public void onResponse(JSONArray response) {
-//                    Log.d("response", response.toString());
-//
-//                }
-//            };
-//
-//            Response.ErrorListener errorListener = new Response.ErrorListener() {
-//                @Override
-//                public void onErrorResponse(VolleyError error) {
-//                    Log.e("error", error.getMessage());
-//                }
-//            };
-//
-//            request = new JsonArrayRequest(Request.Method.GET, url, null, responseListener, errorListener);
-//
-//            requestQueue.add(request);
-//        }
-//    }
-
-
-    public void setInfectedUsers() {
+    public static void setInfectedUsers(Context context) {
 
         try {
-            RequestQueue requestQueue = Volley.newRequestQueue(this);
-            String dbValue = myDB.getPersonalInfoData();
+            RequestQueue requestQueue = Volley.newRequestQueue(context);
+            String dbValue = MainActivity.myDB.getPersonalInfoData();
             String URL = "https://yirlg8c7kc.execute-api.ap-southeast-2.amazonaws.com/prod/data";
             JSONObject jsonBody = new JSONObject();
             jsonBody.put("InfectedUserID", dbValue);
