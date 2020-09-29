f2a4af800c70a8a9e110673729a13c5cf27cd115
Samuel Shingler <samuel.s.shingler@gmail.com>
2020-09-30 11:42:53 +1300

    Material card in dashboard fragment shows stats from ministry of health website, pretty rough atm but will continue to refine

--- Application/app/src/main/java/com/covid/ui/dashboard/DashboardFragment.java
+++ Application/app/src/main/java/com/covid/ui/dashboard/DashboardFragment.java
@@ -4,32 +4,82 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.work.WorkInfo;
+import androidx.work.WorkManager;
 
+import com.covid.MainActivity;
 import com.covid.R;
+import com.covid.utils.TableData;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.Objects;
+import java.util.concurrent.Executor;
+
+import static com.covid.MainActivity.getDataWorkRequest;
+import static com.covid.MainActivity.tableDataArrayList;
+import static com.covid.MainActivity.workManager;
 
 public class DashboardFragment extends Fragment {
 
-    private DashboardViewModel dashboardViewModel;
+    private TextView txtConfirmed;
+    private TextView txtDeaths;
+    private TextView txtRecovered;
+    private ImageView imgRefresh;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        dashboardViewModel =
-                ViewModelProviders.of(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+
+        txtConfirmed = root.findViewById(R.id.txtConfirmed);
+        txtDeaths = root.findViewById(R.id.txtDeaths);
+        txtRecovered = root.findViewById(R.id.txtRecovered);
+        imgRefresh = root.findViewById(R.id.imgRefresh);
+
+        updateTableData();
+
+        imgRefresh.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                final Observer<WorkInfo> workInfoObserver = new Observer<WorkInfo>() {
+                    @Override
+                    public void onChanged(WorkInfo workInfo) {
+                        if (workInfo.getState() == WorkInfo.State.SUCCEEDED) {
+                            updateTableData();
+                        } else if (workInfo.getState() == WorkInfo.State.FAILED) {
+                            Toast.makeText(getContext(), "Failed to retrieve data, please make sure your internet is working and try again", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                };
+                workManager.pruneWork();
+                workManager.enqueue(getDataWorkRequest);
+                LiveData<WorkInfo> status = workManager.getWorkInfoByIdLiveData(getDataWorkRequest.getId());
+                status.observe(getViewLifecycleOwner(), workInfoObserver);
             }
         });
+
         return root;
+    }
+
+    private void updateTableData() {
+        if (tableDataArrayList.size() == 3) {
+            txtConfirmed.setText(tableDataArrayList.get(0).getTotal());
+            txtDeaths.setText(tableDataArrayList.get(1).getTotal());
+            txtRecovered.setText(tableDataArrayList.get(2).getTotal());
+        }
     }
 }
