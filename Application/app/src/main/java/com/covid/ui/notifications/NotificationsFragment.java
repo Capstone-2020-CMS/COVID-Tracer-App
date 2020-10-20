f6083296c372eb78b74fb1521c8d1892ed9d42ba
haywoc01 <haywoc01@myunitec.ac.nz>
2020-10-20 14:33:48 +1300

    Working on delete volley

--- Application/app/src/main/java/com/covid/ui/notifications/NotificationsFragment.java
+++ Application/app/src/main/java/com/covid/ui/notifications/NotificationsFragment.java
@@ -14,6 +14,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.covid.R;
+import com.covid.database.cloud.VolleyDELETE;
 import com.covid.database.cloud.VolleyGET;
 import com.covid.database.cloud.VolleyPOST;
 import com.google.android.material.card.MaterialCardView;
@@ -65,7 +66,8 @@ public class NotificationsFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 expoHandler();
-                VolleyPOST.setInfectedUsers(getContext());
+                //VolleyPOST.setInfectedUsers(getContext());
+                VolleyDELETE.deleteInfectedUser(getContext());
             }
         });
 
